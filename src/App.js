8308f7e85c88f0b7d4a8ed1e7ab3c0ce57f300b8
Raquel_Gomes <raquel.wurzler@hotmail.com>
2022-08-29 15:15:34 -0300

    Question Two Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-import Carteira from './pages/Carteira';
+import Wallet from './pages/Wallet';
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route path="/carteira" component={ Carteira } />
+        <Route path="/carteira" component={ Wallet } />
       </Switch>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+// import store from '../redux/store';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <section>
+        <h4 data-testid="email-field">{ email }</h4>
+        <h4 data-testid="total-field">0</h4>
+        <h4 data-testid="header-currency-field">BRL</h4>
+      </section>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
